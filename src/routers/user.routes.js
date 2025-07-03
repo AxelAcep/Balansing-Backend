b355a84f8eac23fc92522b1ac9e8df92a44f6238
AxelAcep <axelsebayang456@gmail.com>
2025-07-03 22:17:36 +0700

    Forgot Password

--- src/routers/user.routes.js
+++ src/routers/user.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { passport } = require("../passport");
 
-const { login, registerKader, logout } = require("../controllers");
+const { login, registerKader, logout, requestPasswordReset, handleResetPasswordPage, updatePasswordFromForm } = require("../controllers");
 
 const { loginRateLimiter } = require("../middlewares/RateLimit");
 
@@ -10,6 +10,10 @@ const router = express.Router();
 router.post("/registerKader", registerKader);
 router.post("/login", loginRateLimiter, login);
 router.post("/logout", logout);
+
+router.post("/forgetPass", requestPasswordReset );
+router.get('/handleresetpassword', handleResetPasswordPage);
+router.post('/handleresetpassword', updatePasswordFromForm);
 
 router.get("/test1", (req, res) => {
   res.send("Test");
