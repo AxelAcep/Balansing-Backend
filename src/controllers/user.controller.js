02724ee0ca666a94b94cfcfea45818356b2af1b8
AxelAcep <axelsebayang456@gmail.com>
2025-07-04 12:42:22 +0700

    Change Color Reset Password Page

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -287,8 +287,8 @@ const handleResetPasswordPage = async (req, res) => {
                 .form-group { margin-bottom: 15px; text-align: left; }
                 label { display: block; margin-bottom: 5px; color: #555; }
                 input[type="password"] { width: calc(100% - 20px); padding: 10px; border: 1px solid #ddd; border-radius: 4px; font-size: 16px; }
-                button { background-color: #007bff; color: white; padding: 10px 15px; border: none; border-radius: 4px; cursor: pointer; font-size: 16px; width: 100%; }
-                button:hover { background-color: #0056b3; }
+                button { background-color: #9FC86A; color: white; padding: 10px 15px; border: none; border-radius: 4px; cursor: pointer; font-size: 16px; width: 100%; }
+                button:hover { background-color:rgb(173, 199, 140); }
                 .message { margin-top: 20px; padding: 10px; border-radius: 4px; font-weight: bold; }
                 .success { background-color: #d4edda; color: #155724; border: 1px solid #c3e6cb; }
                 .error { background-color: #f8d7da; color: #721c24; border: 1px solid #f5c6cb; }
@@ -296,7 +296,7 @@ const handleResetPasswordPage = async (req, res) => {
         </head>
         <body>
             <div class="container">
-                <h2>Set Password Baru</h2>
+                <h2>Buat Password Baru</h2>
                 <div id="message" class="message" style="display:none;"></div>
                 <form id="resetPasswordForm">
                     <div class="form-group">
